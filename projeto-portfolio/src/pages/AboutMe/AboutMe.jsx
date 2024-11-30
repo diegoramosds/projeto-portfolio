4797c3502a6222069eb3c48c785d00bf797896f1
Diego <diegoramos3709@gmail.com>
2024-11-30 15:43:50 -0300

    Página about

--- projeto-portfolio/src/pages/AboutMe/AboutMe.jsx
+++ projeto-portfolio/src/pages/AboutMe/AboutMe.jsx
@@ -4,21 +4,58 @@ import profileImage  from "../../img/foto-perfil.png"
 
 import 'aos/dist/aos.css';
 
-const AboutMe = () => {
+const AboutMe = () => { 
 
   return (
-    <div className='about-text' data-aos="fade-right" data-aos-delay="100">
-      <div className='text-details'>
+    <div className='about-text' id='about'>
+      <div className='text-details' data-aos="fade-right">
       <h2><span>&lt;&gt;</span>Olá, Me chamo DIEGO!<span>&lt;/&gt;</span></h2>
-        <p>Sou um desenvolvedor com habilidades em construção de layouts responsivos e estilos visualmente atraentes.
-           Tenho aprendido sobre desenvolvimento de interfaces de usuário dinâmicas e interativas e consumo de api,
-           atualmente estou cursando <strong className='important-name'>Análise e Desenvolvimento de Sistemas</strong> na faculdade. Paralelamente, tenho aproveitado diversas oportunidades 
-            de aprendizado online para ampliar meus conhecimentos.</p>
+        <p>Atualmente, estou me aprofundando no desenvolvimento <strong className='important-name'> Front-end</strong>, com foco em práticas 
+          modernas e consumo de <strong className='important-name'> APIs </strong> para criar interfaces dinâmicas e interativas. Estou cursando 
+          <strong className='important-name'> Análise e Desenvolvimento de Sistemas </strong> , o que tem sido uma ótima oportunidade para construir 
+          uma base sólida em programação e estrutura de dados, além de explorar aspectos fundamentais 
+          do <strong className='important-name'>desenvolvimento de software</strong>. Paralelamente, tenho aproveitado diversos recursos e
+          <strong className='important-name'> cursos online</strong>,
+           que me permitem explorar novas tecnologias, <strong className='important-name'>frameworks </strong> 
+          e técnicas avançadas, expandindo 
+          constantemente meus conhecimentos e aprimorando minhas habilidades na área.</p>
       </div>
         <div className='image-container'>
           <img src={profileImage} alt="Foto de perfil"/>
         </div>
-    </div>
+         <div className='about-memory'>
+
+           <div className='memory-details' data-aos="fade-right" >
+           <h6>Meu Primeiro Contato com a Programação</h6>
+
+            <p>Comecei na programação focando em algoritmos e lógica, explorando 
+              <strong className='important-name'> HTML e CSS</strong> para
+               construir as primeiras páginas e entender como tudo funcionava na 
+               <strong className='important-name'> web</strong> .</p>
+            </div>
+           <div className='memory-details' data-aos="fade-up" >
+          
+            <h6>Aprofundando em HTML, CSS e JavaScript</h6>
+
+            <p>Aprofundei meus conhecimentos em <strong className='important-name'> HTML e CSS</strong>, e então 
+            acabei evoluindo para o <strong className='important-name'>JavaScript</strong>,
+              que abriu portas para criar interações mais dinâmicas e funcionais.</p>
+           </div>
+           <div className='memory-details' data-aos="fade-left">
+           <h6>React e Carreira no Front-End</h6>
+
+            <p>Após dominar o <strong className='important-name'>JavaScript</strong>, iniciei estudos em
+            <strong className='important-name'> React</strong>  e decidi ingressar 
+              na faculdade de <strong className='important-name'>Análise e Desenvolvimento de Sistemas </strong> 
+              com foco contínuo em <strong className='important-name'> front-end</strong>.</p>
+            </div>
+         </div>
+         
+         <p><a href="/cv-diego_ramos.pdf" target="_blank" rel="noopener noreferrer">
+          DOWLOAD CV</a></p>
+       
+         </div>
+
     
   )
 }
