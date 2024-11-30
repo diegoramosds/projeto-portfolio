087c9a61578345f300a1b744862259bcd67261be
Diego <diegoramos3709@gmail.com>
2024-11-30 15:45:53 -0300

    Footer

--- projeto-portfolio/src/Components/Footer/Footer.jsx
+++ projeto-portfolio/src/Components/Footer/Footer.jsx
@@ -9,11 +9,26 @@ const Footer = () => {
   return (
     <footer>
       <div className='icons-footer'>
-        <p>Minhas redes <span>🡻</span></p>
-        <a href="https://github.com/diegoramosds" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/diego-rds/" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a>
-        <a href="mailto:diegoramos370@gmail.com" target="_blank" rel="noopener noreferrer"><MdEmail/></a>
-        <a href="https://www.instagram.com/_diegoo_66/" target="_blank" rel="noopener noreferrer"><FaInstagram/></a>
+        <h2>Minhas redes</h2>
+        <a href="https://github.com/diegoramosds" 
+        target="_blank" rel="noopener noreferrer" 
+        aria-label="Visite meu GitHub">
+        <FaGithub/></a>
+
+        <a href="https://www.linkedin.com/in/diego-rds/"
+         target="_blank" rel="noopener noreferrer"
+          aria-label="Visite meu LinkedIn">
+            <FaLinkedin/></a>
+            
+            <a href="mailto:diegoramos370@gmail.com" 
+            target="_blank" rel="noopener noreferrer"
+            aria-label="Mande um email">
+              <MdEmail/></a>
+
+              <a href="https://www.instagram.com/_diegoo_66/" 
+              target="_blank" rel="noopener noreferrer"
+               aria-label="Visite meu Instagram">
+                <FaInstagram/></a>
       </div>
          <h3>Portfólio Dev!</h3>
          <p>Diego Ramos &copy; 2024</p>
