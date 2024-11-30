5df1facf11a69c43cedd610133770074ec24b47c
Diego <diegoramos3709@gmail.com>
2024-11-30 15:43:01 -0300

    Página projects

--- projeto-portfolio/src/pages/Projects/Projects.jsx
+++ projeto-portfolio/src/pages/Projects/Projects.jsx
@@ -1,11 +1,14 @@
 import './Projects.css';
 
+import donitelas from '../../img/donitelas.png';
 import reactLoja from '../../img/React.png';
 import reactAluno from '../../img/ReactAlunos.png';
 import reactAgenda from '../../img/projeto-agenda.png';
 import reactParticipantes from '../../img/reactParticipantes.png';
 import reactViagens from '../../img/reactviagens.png';
-import reactblog from '../../img/reactblog.png';
+import reactBlog from '../../img/reactblog.png';
+import reactSecretWord from '../../img/secret.png';
+import reactInOrbit from '../../img/in.orbit.png';
 
 import { useEffect } from 'react';
 import AOS from 'aos';
@@ -23,11 +26,30 @@ function Projects() {
       <section id='projects'>
         <h2>Meus Projetos</h2>
         <div className='projetos-container'>
+        <article className="img-item" data-aos="fade-right">
+            <img src={donitelas} alt="Projeto-Blog" loading="lazy" />
+            <div className='infodetails'>
+            <h3>DONITELAS</h3>
+              <p>Este projeto de site foi desenvolvido para a Donitelas, uma empresa especializada 
+                em instalação e manutenção de telas, cercas e grades de segurança.</p>
+              <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>vite</li>
+                <li>Bootstrap</li>
+                <li>Html e css</li>  
+                <li>Scss</li>  
+              </ul>
+            
+            <a href="https://github.com/diegoramosds/projeto-donitelas" target='blank'>Projeto completo Aqui</a>
+            </div>
+          </article>
           <article className="img-item" data-aos="fade-right">
-            <img src={reactblog} alt="Projeto-Blog" loading="lazy" />
+            <img src={reactBlog} alt="Projeto-Blog" loading="lazy" />
             <div className='infodetails'>
             <h3>MiniBlog</h3>
-              <p>Este projeto foi construído com React, utilizando Firebase para autenticação e banco de dados. O axios gerencia as requisições API e o React Router é responsável pela navegação entre as páginas, garantindo uma experiência de usuário fluida e segura</p>
+              <p>Criação e edição de postagens. Permite aos usuários adicionar e modificar conteúdo.
+                  Garantia de que apenas usuários autorizados possam criar e editar postagens</p>
               <ul>
                 <li>JavaScript</li>
                 <li>React</li>
@@ -40,11 +62,13 @@ function Projects() {
             </div>
           </article>
 
-          <article className="img-item" data-aos="fade-left">
+          <article className="img-item" data-aos="fade-right">
             <img src={reactViagens} alt="Projeto-Viagens" loading="lazy" />
             <div className='infodetails'>
-            <h3>Viagens</h3>
-              <p>Este projeto é uma aplicação web de detalhes de viagem desenvolvida no evento NLW da Rocketseat, com funcionalidades para gerenciar links importantes de forma organizada e responsiva durante o planejamento e execução de viagens.</p>
+            <h3>Plann.er</h3>
+              <p> Detalhes de viagem desenvolvida no evento NLW da 
+                Rocketseat, gerencia informações importantes de forma organizada 
+                 durante o planejamento de viagens</p>
             <ul>
               <li>JavaScript</li>
               <li>React</li>
@@ -60,8 +84,9 @@ function Projects() {
           <article className="img-item" data-aos="fade-right">
             <img src={reactLoja} alt="Projeto-Loja" loading="lazy" />
             <div className='infodetails'>
-            <h3>Loja Online</h3>
-              <p>Este projeto é uma loja online de roupas onde os usuários podem adicionar, ajustar e remover produtos do carrinho. A gestão do carrinho é feita com Redux para uma experiência dinâmica e responsiva.</p>
+            <h3>Redux Shopping</h3>
+              <p>Loja online de roupas onde os usuários podem adicionar, ajustar e 
+                remover produtos do carrinho</p>
             <ul>
               <li>JavaScript</li>
               <li>React</li>
@@ -73,15 +98,15 @@ function Projects() {
             
           </article>
 
-          <article className="img-item" data-aos="fade-left">
+          <article className="img-item" data-aos="fade-right">
             <img src={reactAluno} alt="Projeto-Aluno" loading="lazy" />
             <div className='infodetails'>
             <h3>Gerenciamento de Alunos</h3>
-              <p>O projeto é uma aplicação web para gerenciamento de informações de alunos, permitindo adicionar, editar, excluir registros e trocar a foto do aluno. Foi desenvolvido com React no frontend e Node.js no backend para gerenciamento de dados.</p>
+              <p>Gerenciamento de informações de alunos, permitindo 
+                adicionar, editar, excluir registros e trocar a foto do aluno</p>
             <ul>
               <li>JavaScript</li>
               <li>React</li>
-              <li>HTML</li>
               <li>Node</li>
             </ul>
             <a href="https://github.com/diegoramosds/projeto-alunos" target='blank'>Projeto completo Aqui </a>
@@ -94,11 +119,11 @@ function Projects() {
              
               <div className='infodetails'>
               <h3>Agenda de Contatos</h3>
-                <p>A aplicação permite a criação de contas de usuários, adição de contatos associados a essas contas e remoção automática dos contatos quando excluídos pelo usuário, garantindo consistência com o banco de dados MongoDB.</p>
+                <p>A aplicação permite a criação de contas de usuários, adição de contatos associados 
+                  a essas contas e remoção automática dos contatos quando excluídos pelo usuário</p>
               <ul>
                 <li>JavaScript</li>
                 <li>React</li>
-                <li>HTML</li>
                 <li>Node</li>
                 <li>Express</li>
                 <li>MONGODB</li>
@@ -109,12 +134,14 @@ function Projects() {
               
           </article>
 
-          <article className="img-item" data-aos="fade-left">
+          <article className="img-item" data-aos="fade-right">
             <img src={reactParticipantes} alt="Projeto-Participantes" loading="lazy" />
             <div className='infodetails'>
             <h3>Lista de Participantes</h3>
 
-              <p>Este projeto, desenvolvido em React, é uma lista de participantes de um evento. Permite aos usuários buscar participantes, navegar pelas páginas de resultados e visualizar detalhes como código, nome, e-mail, data de inscrição e data de check-in, quando disponível.</p>
+              <p>Lista de participantes de um evento,
+                permite aos usuários buscar participantes, navegar pelas páginas de resultados e 
+                visualizar detalhes da busca</p>
             <ul>
               <li>JavaScript</li>
               <li>TypeScript</li>
@@ -126,6 +153,41 @@ function Projects() {
             <a href="https://github.com/diegoramosds/projeto-participantes" target='blank'>Projeto completo Aqui</a>
             </div>
             
+          </article>
+
+          
+          <article className="img-item" data-aos="fade-right">
+            <img src={reactInOrbit} alt="Projeto-InOrbit" loading="lazy" />
+            <div className='infodetails'>
+            <h3>In.Orbit</h3>
+              <p>Gestão de metas semanais feita em um evento, da Rocketseat.
+                 A aplicação possui funcionalidades como a criação, acompanhamento e conclusão de metas semanais</p>
+            <ul>
+              <li>Typescript</li>
+              <li>React</li>
+              <li>PostgreSQL</li>
+              <li>Node</li>
+            </ul>
+            <a href="https://github.com/diegoramosds/projeto-in.orbit" target='blank'>Projeto completo Aqui </a>
+            </div>
+          </article>
+
+          <article className="img-item" data-aos="fade-right">
+            <img src={reactSecretWord} alt="Projeto-SecretWord" loading="lazy" />
+            <div className='infodetails'>
+            <h3>SecretWord</h3>
+              <p>Jogo, onde o jogador deve descobrir
+                 uma palavra secreta com base em uma dica de categoria.
+                 verifica letras adivinhadas, controla tentativas e permite reiniciar após o término</p>
+            <ul>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Html</li>
+              <li>CSS</li>
+
+            </ul>
+            <a href="https://github.com/diegoramosds/projeto-secret_word" target='blank'>Projeto completo Aqui </a>
+            </div>
           </article>
         </div>
       </section>
