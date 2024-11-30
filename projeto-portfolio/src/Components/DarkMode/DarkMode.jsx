e86f93d38b8e697bcd1f4a0d8754707c6ecc5624
Diego <diegoramos3709@gmail.com>
2024-11-30 15:45:02 -0300

    Configurando modo claro

--- projeto-portfolio/src/Components/DarkMode/DarkMode.jsx
+++ projeto-portfolio/src/Components/DarkMode/DarkMode.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
-import { FaMoon, FaSun } from 'react-icons/fa';
-
+import { useState, useEffect } from 'react';
+import { FaLightbulb, FaMoon } from 'react-icons/fa';
 import './DarkMode.css';
-const DarkMode = () => {
-    const [darkMode, setDarkMode] = useState(false);
 
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-        if (darkMode) {
-          document.body.classList.remove('dark-mode');
-        } else {
-          document.body.classList.add('dark-mode');
-        }
-      };
+const DarkMode = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true';
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark-mode', darkMode);
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <div className="dark_mode" onClick={toggleDarkMode}>
-       {darkMode ? <FaSun title='Modo claro' className='model_sun'/>  : <FaMoon title='Modo escuro' className='model_moon'/>}
+      {darkMode ? (
+        <FaMoon title="Modo claro" className="icon rotate" />
+      ) : (
+        <FaLightbulb title="Modo escuro" className="icon rotate" />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default DarkMode;
