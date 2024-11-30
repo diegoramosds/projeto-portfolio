c217c682340d9672128b362244cce452e9d11cd3
Diego <diegoramos3709@gmail.com>
2024-11-30 15:45:44 -0300

    Navbar

--- projeto-portfolio/src/Components/NavBar/NavBar.jsx
+++ projeto-portfolio/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
 import './NavBar.css';
+import { FaBars, FaTimes  } from 'react-icons/fa';
+import DarkMode from '../DarkMode/DarkMode';
 
 
 function Header() {
-  
   function scrollToSkills() {
     closeMenu();
     const skillsElement = document.getElementById('skills');
@@ -27,37 +28,41 @@ function Header() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
     if (!isOpen) {
-      document.body.style.overflow = 'hidden'; // Remove rolagem
-      document.body.style.position = 'fixed';  // Impede rolagem por toque
-    } else {
-      document.body.style.overflow = 'auto';   // Restaura rolagem
-      document.body.style.position = 'relative';  // Permite rolagem por toque
+      document.body.style.overflow = 'hidden'; 
+      document.body.style.position = 'fixed';
+    }  else {
+     
+      document.body.style.overflowY = 'auto';
+      document.body.style.position = '';  
     }
   };
   
   const closeMenu = () => {
     setIsOpen(false);
-    document.body.style.overflow = 'auto';     // Restaura rolagem
-    document.body.style.position = 'relative'; // Permite rolagem por toque
+    document.body.style.overflowY = 'auto';  
+    document.body.style.position = '';  
   }
-  
     return (
 
       <div className="header-container">
-         {isOpen && <div className="overlay" onClick={closeMenu}></div>}
+        <div className='header-background'>
+         <p><span><DarkMode/></span><strong className='important-name'>DR</strong></p>
+         
+          {isOpen && <div className="overlay" onClick={closeMenu}></div>}
 
-         {isOpen ?  <div className="hamburger" onClick={toggleMenu}> X </div>
-         : <div className="hamburger" onClick={toggleMenu}>  &#9776;  </div>
-        }
-        <div className={`overlay ${isOpen ? 'open' : ''}`} onClick={closeMenu}>
-        <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
-        
-        <a  onClick={scrollToSkills} className="custom-link skill">Habilidades</a>
-        <a onClick={scrollToProjects} className="custom-link project">Projetos</a>
-        <a onClick={scrollToContact} className="custom-link cont">Contato</a>
-        </nav>
+          {isOpen ?  <div className="hamburger" onClick={toggleMenu}><FaTimes className="icon rotate"/> </div>
+          : <div className="hamburger" onClick={toggleMenu}><FaBars className="icon rotate"/> </div>
+          }
+      
+          <div className={`overlay ${isOpen ? 'open' : ''}`} onClick={closeMenu}>
+            <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
+            <a  onClick={scrollToSkills} className="custom-link skill">Habilidades</a>
+            <a onClick={scrollToProjects} className="custom-link project">Projetos</a>
+            <a onClick={scrollToContact} className="custom-link cont">Contato</a>
+            </nav>
+          </div>
         </div>
-
+ 
       </div>
       
     );
