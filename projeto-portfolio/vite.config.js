9474be40cc4c5e8cffc9b103f9e188decf07aa71
Diego <diegoramos3709@gmail.com>
2025-04-01 22:16:13 -0300

    Update vite.config.js

--- projeto-portfolio/vite.config.js
+++ projeto-portfolio/vite.config.js
@@ -1,33 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   build: {
-    sourcemap: false, 
+    sourcemap: false,
     rollupOptions: {
       output: {
-
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
         },
       },
     },
-    minify: 'terser',
-    chunkSizeWarningLimit: 1000, 
+    minify: "terser",
+    chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
-  
-    include: ['react', 'react-dom'],
+    include: ["react", "react-dom"],
   },
   server: {
-    
     open: true,
   },
 });
