f56087debc005fc0e20cd67732a53d2e069514fb
Diego <diegoramos3709@gmail.com>
2025-04-03 22:40:46 -0300

    Update App.jsx

--- projeto-portfolio/src/App.jsx
+++ projeto-portfolio/src/App.jsx
@@ -1,13 +1,12 @@
-import Skills from './pages/Skills/Skills'
-import NavBar  from './Components/NavBar/NavBar'
-import Projects from './pages/Projects/Projects'
-import Contact from './pages/Contact/Contact'
+import Skills from "./pages/Skills/Skills";
+import NavBar from "./Components/NavBar/NavBar";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
 
-import { BrowserRouter} from 'react-router-dom'
-import Footer from './Components/Footer/Footer'
-import Home from './pages/Home/Home'
-import AboutMe from './pages/AboutMe/AboutMe'
-
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import AboutMe from "./pages/AboutMe/AboutMe";
 
 const App = () => {
   return (
@@ -20,7 +19,7 @@ const App = () => {
       <Contact />
       <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
